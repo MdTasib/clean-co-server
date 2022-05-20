edd49a70e468dcf1eca0dd15d4cbb4e59ef83d38
MdTasib <ohidulalam442@gmail.com>
2022-05-20 19:28:25 +0600

    update & delete api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -23,9 +23,41 @@ async function run() {
 		await client.connect();
 		const serviceCollection = client.db("cleanCo").collection("services");
 
-		app.get("/service", async (req, res) => {
+		// get all service
+		app.get("/get-service", async (req, res) => {
 			const service = await serviceCollection.find().toArray();
 			res.send(service);
+		});
+
+		// post a single service data
+		app.post("/add-service", async (req, res) => {
+			const service = req.body;
+			const result = await serviceCollection.insertOne(service);
+			res.send(result);
+		});
+
+		// update a single service
+		app.put("/update-service/:id", async (req, res) => {
+			const id = req.params.id;
+			const service = req.body;
+			const filter = { _id: ObjectId(id) };
+			const options = { upsert: true };
+			const updatedDoc = { $set: service };
+
+			const result = await serviceCollection.updateOne(
+				filter,
+				updatedDoc,
+				options
+			);
+			res.send(result);
+		});
+
+		// delete a single service
+		app.delete("/delete-service/:id", async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: ObjectId(id) };
+			const result = await serviceCollection.deleteOne(query);
+			res.send(result);
 		});
 	} finally {
 	}
